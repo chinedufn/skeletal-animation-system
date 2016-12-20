376161ee0a195b49089f9baa257c41c80eaf7f0c
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-12-20 07:04:14 -0500

    Add fork me on GitHub banner

--- demo/render-controls.js
+++ demo/render-controls.js
@@ -9,31 +9,49 @@ var animationRanges = {
 
 function renderControls (State) {
   var controls = h('div', {
-    style: {
-      display: 'flex',
-      'align-items': 'center',
-      height: '100px'
-    }
   }, [
-    h('button', {
+    h('div', {
       style: {
-        cursor: 'pointer',
-        height: '30px'
-      },
-      onclick: function () {
-        State.set('previousAnimation', {
-          range: animationRanges[State.get().currentAnimation.name],
-          startTime: State.get().currentAnimation.startTime
-        })
-
-        var newAnimationName = State.get().currentAnimation.name === 'dance' ? 'bend' : 'dance'
-        State.set('currentAnimation', {
-          name: newAnimationName,
-          range: animationRanges[newAnimationName],
-          startTime: State.get().currentTime
-        })
+        display: 'flex',
+        'align-items': 'center',
+        width: '100%',
+        height: '100px'
       }
-    }, 'Change animation')
+    }, [
+      h('button', {
+        style: {
+          cursor: 'pointer',
+          height: '30px'
+        },
+        onclick: function () {
+          State.set('previousAnimation', {
+            range: animationRanges[State.get().currentAnimation.name],
+            startTime: State.get().currentAnimation.startTime
+          })
+
+          var newAnimationName = State.get().currentAnimation.name === 'dance' ? 'bend' : 'dance'
+          State.set('currentAnimation', {
+            name: newAnimationName,
+            range: animationRanges[newAnimationName],
+            startTime: State.get().currentTime
+          })
+        }
+      }, 'Change animation')
+    ]),
+    h('a', {
+      href: 'https://github.com/chinedufn/skeletal-animation-system'
+    }, [
+      h('img#fork-github', {
+        style: {
+          alt: 'Fork me on GitHub',
+          border: 0,
+          position: 'absolute',
+          right: 0,
+          top: 0
+        },
+        src: 'fork-github.png'
+      })
+    ])
   ])
 
   return controls
